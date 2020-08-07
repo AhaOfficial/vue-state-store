d86461dc534bd37c2b2a0e24db308c03f35ad1a2
hm-lee <hm.lee@a-ha.io>
2020-08-07 19:33:06 +0900

    Remove unused functions

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import * as Utils from './utils'
 import { Store } from './store'
 
 /**
- * Create a `Writable` store that allows both updating and reading by subscription.
+ * Create a store that allows both updating and reading by subscription.
  * @param {*=}value initial value
  * @param {StartStopNotifier=}start start and stop notifications for subscriptions
  */
--- src/utils.ts
+++ src/utils.ts
@@ -1,29 +1,7 @@
 export const noop = () => {}
 
-export const identity = (x) => x
-
-export const assign = <T, S>(tar: T, src: S): T & S => {
-    // @ts-ignore
-    for (const k in src) tar[k] = src[k]
-    return tar as T & S
-}
-
-export const isPromise = <T = any>(value: any): value is PromiseLike<T> => {
-    return (
-        value && typeof value === 'object' && typeof value.then === 'function'
-    )
-}
-
 export const run = (callback: any) => {
     return callback()
-}
-
-export const blankObject = () => {
-    return Object.create(null)
-}
-
-export const runAll = (fns) => {
-    fns.forEach(run)
 }
 
 export const isFunction = (thing: any): thing is Function => {
@@ -38,10 +16,6 @@ export const safeNotEqual = (a, b) => {
 
 export const notEqual = (a, b) => {
     return a != a ? b == b : a !== b
-}
-
-export const isEmpty = (obj) => {
-    return Object.keys(obj).length === 0
 }
 
 export const validateStore = (store, name) => {
@@ -59,26 +33,4 @@ export const getStoreValue = (store) => {
     let value
     subscribe(store, (_) => (value = _))()
     return value
-}
-
-export const once = (fn) => {
-    let ran = false
-    return function (this: any, ...args) {
-        if (ran) return
-        ran = true
-        fn.call(this, ...args)
-    }
-}
-
-export const nullToEmpty = (value) => {
-    return value == null ? '' : value
-}
-
-export const hasProp = (obj, prop) =>
-    Object.prototype.hasOwnProperty.call(obj, prop)
-
-export const actionDestroyer = (action_result) => {
-    return action_result && isFunction(action_result.destroy)
-        ? action_result.destroy
-        : noop
 }
